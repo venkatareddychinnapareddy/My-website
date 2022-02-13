1579cc3b4d384bd790b25640ee7ee68a0742af9c
venkata reddy <72429150+venkatareddychinnapareddy@users.noreply.github.com>
2022-02-13 11:15:33 +0530

    Update docker_pull.sh

--- myscripts/docker_pull.sh
+++ myscripts/docker_pull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Pulling from Amazon ECR..."
-docker pull 509929911430.dkr.ecr.us-east-2.amazonaws.com/dockercicd:latest
+docker pull docker push 509929911430.dkr.ecr.us-east-2.amazonaws.com/mywebsite:latest
