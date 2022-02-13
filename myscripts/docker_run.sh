b7a5f3f05b457e1e1a81c5494e3b162656656e4d
venkata reddy <72429150+venkatareddychinnapareddy@users.noreply.github.com>
2022-02-13 11:16:16 +0530

    Update docker_run.sh

--- myscripts/docker_run.sh
+++ myscripts/docker_run.sh
@@ -2,5 +2,5 @@
 echo "Running container from pulling image from Amazon ECR..."
 docker stop my-app
 docker rm my-app 
-docker run --name my-app -d -p 80:80 509929911430.dkr.ecr.us-east-2.amazonaws.com/dockercicd:latest
+docker run --name my-app -d -p 80:80 509929911430.dkr.ecr.us-east-2.amazonaws.com/mywebsite:latest
 
